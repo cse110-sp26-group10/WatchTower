850146b569d316a6b83a487ac2c264f9eb277960
arw008-droid <arw008@ucsd.edu>
2026-05-29 21:21:56 -0700

    Fix additional merge conflicts

--- src/prototype/tracker/watchtower.js
+++ src/prototype/tracker/watchtower.js
@@ -6,15 +6,6 @@ const baseUrl = isLocal
 const scripts = ["src/prototype/tracker/assets/tracker.js"];
 
 scripts.forEach((src) => {
-<<<<<<< HEAD
-    const scriptName = src.split('/').at(-1);
-    const script = document.createElement('script');
-    script.setAttribute("data-apikey", document.currentScript.getAttribute("data-apikey"));
-    script.src = `${baseUrl}/${src}`;
-    script.onload = () => console.log(`WatchTower: ${scriptName} loaded`);
-    script.onerror = () => console.error(`WatchTower: Failed to load ${scriptName}`);
-    document.head.appendChild(script);
-=======
   const scriptName = src.split("/").at(-1);
   const script = document.createElement("script");
   script.src = `${baseUrl}/${src}`;
@@ -22,5 +13,4 @@ scripts.forEach((src) => {
   script.onerror = () =>
     console.error(`WatchTower: Failed to load ${scriptName}`);
   document.head.appendChild(script);
->>>>>>> 614c9ed06fd99d8dae2767c77855a2adc2f9d235
 });
